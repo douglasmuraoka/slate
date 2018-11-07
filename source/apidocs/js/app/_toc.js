f4f81e74cee94aeeecfd9254a2825661208581ec
Douglas Muraoka <douglas.muraoka@gmail.com>
2018-11-07 15:33:36 -0200

    fix: Remove h3 from toc

--- source/apidocs/js/app/_toc.js
+++ source/apidocs/js/app/_toc.js
@@ -121,7 +121,6 @@
           document.title = originalTitle;
         }
       }
-      loadSubToc()
     };
 
     var makeToc = function() {
@@ -148,17 +147,10 @@
       $(window).resize(debounce(recacheHeights, 200));
     };
 
-    // Function to show the active subtoc and hide others
-    var loadSubToc = function() {
-      $(".toc-h3").parent().parent().css( "display", "none" );
-      $(".toc-h3.active").parent().parent().css( "display", "block" );
-    }
-
     makeToc();
 
     window.recacheHeights = recacheHeights;
     window.refreshToc = refreshToc;
-    window.loadSubToc  = loadSubToc ;
   }
 
   window.loadToc = loadToc;
